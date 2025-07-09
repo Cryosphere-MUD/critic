4926405438266a6b447adce7ad7ad3363201a8df
AbigailBrady <elcathriel@gmail.com>
2025-07-10 00:50:16 +0100

    specific tests that three luau-isms are rejected (#7)
    * specific tests that three luau-isms are rejected
    
    (this is minus a test that `blah` errors - an upstream bug (which i am hoping to at some point make a fix for - when I get my head round antlr, which could be a while) - means we cannot reliably detect this, for now.
    
    https://github.com/boolangery/py-lua-parser/issues/68

---
+++ standalone-tests/failing/luau-inline-ifs.lua
@@ -0,0 +1,4 @@
+-- this is an example of syntax accepted by robox's
+-- "luau" fork which must be rejected by a validator
+-- targetting mainline lua.
+local maxValue = if a > b then a else b
---
+++ standalone-tests/failing/luau-table-iteration.lua
@@ -0,0 +1,4 @@
+-- this a robox/luau-ism
+local a = {"a", "b", "c"}
+for k, v in a do
+end
